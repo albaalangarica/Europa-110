03a609c4a2e07db0e98742c84eb36e1563dc3156
albaalangarica <albalangarica@icloud.com>
2026-09-12 16:09:36 +0200

    Update APPS_SCRIPT_URL to new endpoint

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyvRoDLssoOj3GgMs_1wnbD48ZFwe74rP01CZJ6PG7fXIiQerOxucltKJhA62Zvs8Pbxg/exec';
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzLZ001xb0wl6CIhJ_dSPb5ehP9ej5kn5bQYXmGGlATMn-vYHtZkaOSD3KZRyeiOZM/exec';
